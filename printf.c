579430791213c81daef9a88bd079d3b58858288d
JumokeJay <Jayrevam@gmail.com>
2023-08-25 02:06:20 -0700

    checkaa

---
+++ printf.c
@@ -0,0 +1,52 @@
+#include "main.h"
+int _printf(const char *format, ...);
+/*
+ * _printf - the function to print
+ * @format: the format
+ * Return: the result of the code
+ */
+int _printf(const char *format, ...)
+{
+	va_list args;
+	va_start(args, format);
+
+	int count = 0;
+
+	while (*format != '\0')
+	{
+	if (*format == '%')
+	{
+	format++; // Move past the '%'
+	if (*format == 'c')
+	{
+	char c = (char)va_arg(args, int);
+	putchar(c);
+	count++;
+	}
+	else if (*format == 's')
+	{
+	char *str = va_arg(args, char *);
+	while (*str != '\0')
+	{
+	putchar(*str);
+	str++;
+	count++;
+	}
+	}
+	else if (*format == '%')
+	{
+	putchar('%');
+	count++;
+	}
+	}
+	else
+	{
+	putchar(*format);
+	count++;
+	}
+	format++;
+	}
+
+	va_end(args);
+	return count;
+}
